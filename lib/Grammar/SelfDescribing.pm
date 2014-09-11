b4e24df0ab1c250459deff28e91f359fd5ad6f2d
Matthias Kling <m.kling@sprax.eu>
2014-09-12 01:24:18 +0200

    + SelfDescribing can parse up to single-quoted string def (problem is escaping)

--- lib/Grammar/SelfDescribing.pm
+++ lib/Grammar/SelfDescribing.pm
@@ -4,19 +4,26 @@ use Grammar::Tracer;
 
 
 grammar SelfDescribing {
-    rule  TOP           { [ <comment> | <type-decl> | <statement> ]* }
+    rule  TOP           { \s* [ <comment> | <type-decl> | <statement> ]* }
     token comment       { '#' \N* $$ }
     rule  statement     { [ <use-stmt> | <say-stmt> | <term> ] ';' }
     rule  use-stmt      { 'use' [ <version> | <module-ident> ] }
     rule  say-stmt      { 'say' <term>? }
     token version       { 'v' \d+ ['.' \d+]? }
-    token module-ident  { <alpha>+ ['::' <alpha>+]* }
+    token module-ident  { <bare-symbol> ['::' <bare-symbol>]* }
     token term          { [ <symbol> | <literal> ] <indirection>? }
     token symbol        { <bare-symbol> | <sig-symbol> }
-    token bare-symbol   { [ <alpha> | '_' ] [ '-' | '_' | <alpha> ]* }
+    token bare-symbol   { <ident> [ '-' <ident>? ]* }
     token sig-symbol    { <sig-twig> <bare-symbol> }
     token sig-twig      { ['$' | '@' | '%'] ['*']? }
-    token literal       { \d+ } # <<<<<<<<<<
+    token literal       { <number> | <string> }
+    token number        { \d+ ['.' \d+]? }
+    token string        { <sq-str> }
+
+    #----------------------------------------
+    token sq-str        { '\'' [ <-[\'\n\\]>+ | <sq-str-esc> ]* '\'' }
+    token sq-str-esc    { '\\' <[\'\\bfnrt]> }
+
     token indirection   { 
         [
         | <method-call>
@@ -32,22 +39,48 @@ grammar SelfDescribing {
     rule  regular-code  { [ 'sub' | 'method' | 'submethod' ] <bare-symbol>? <regular-code-body>  }
     rule  class-like-body { '{' <TOP> '}' }
     rule  regular-code-body { '{' <TOP> '}' }
-    rule  production-body   { '{' [ <regex-body> | <regular-code-body> ]* '}' }
-    rule  regex-body    { [ [ <rx-call> | <rx-lit> | <rx-anchor> ] <rx-quant>? ]+ }
-    token  rx-call      { '<' <bare-symbol> '>' }
-    token rx-lit        { '\\d' | '\\N' }
+    rule  production-body   { '{' [ <rx> | <regular-code-body> ]* '}' }
+    rule  rx            { <rx-term> [ '|' <rx-term> ]* }
+    rule  rx-term       { <rx-factor>+ }
+    rule  rx-factor     {
+        [ <rx-lit>
+        | <rx-anchor>
+        | <rx-call>
+        | '[' <rx> ']'
+        | '(' <rx> ')'
+        ]
+        <rx-quant>?
+    }
+    token rx-call       { '<' <bare-symbol> '>' }
+    token rx-lit        { '\\' [<alpha> | '\'']  |  <string> }
     token rx-anchor     { '^^' | '^' | '$$' | '$' }
     token rx-quant      { '?' | '*' | '+' }
+
 }
 
-say SelfDescribing.parse(q:to/ENDOFTEXT/);
+say SelfDescribing.parse(:rule<TOP>, q:to/ENDOFTEXT/);
 use v6; # this is Perl6
 
-use Grammar::Debugger;
+#use Grammar::Tracer;
+
 
 grammar SelfDescribing {
-    rule  TOP           { <comment> }
+    rule  TOP           { \s* [ <comment> | <type-decl> | <statement> ]* }
+    token comment       { '#' \N* $$ }
+    rule  statement     { [ <use-stmt> | <say-stmt> | <term> ] ';' }
+    rule  use-stmt      { 'use' [ <version> | <module-ident> ] }
+    rule  say-stmt      { 'say' <term>? }
+    token version       { 'v' <number> }
+    token module-ident  { <bare-symbol> ['::' <bare-symbol>]* }
+    token term          { [ <symbol> | <literal> ] <indirection>? }
+    token symbol        { <bare-symbol> | <sig-symbol> }
+    token bare-symbol   { <ident> [ '-' <ident>? ]* }
+    token sig-symbol    { <sig-twig> <bare-symbol> }
+    token sig-twig      { ['$' | '@' | '%'] ['*']? }
+    token literal       { <number> | <string> }
+    token number        { \d+ ['.' \d+]? }
+    token string        { <sq-str> }
 }
-
-say SelfDescribing.parsefile($*FILE);
+    #----------------------------------------
 ENDOFTEXT
+
