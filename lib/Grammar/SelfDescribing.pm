f880d1f5a7bd9d0db50b89f7aa3dffe0a5277471
Matthias Kling <m.kling@sprax.eu>
2014-09-12 21:07:37 +0200

    * fixed single-quoted and double-quoted strings, added make-stmt and exit-stmt, added negative number literals, added token rx-angle-brkt (for rx-call, rx-cclass [negated or non-negated] and local rename [ eg <s=subrule> ])

--- lib/Grammar/SelfDescribing.pm
+++ lib/Grammar/SelfDescribing.pm
@@ -1,36 +1,55 @@
 use v6; # this is Perl6
-
 use Grammar::Tracer;
-
 
 grammar SelfDescribing {
     rule  TOP           { \s* [ <comment> | <type-decl> | <statement> ]* }
     token comment       { '#' \N* $$ }
-    rule  statement     { [ <use-stmt> | <say-stmt> | <term> ] ';' }
+    rule  statement     { [ <use-stmt> | <say-stmt> | <make-stmt>| <exit-stmt> | <term> ] ';' }
     rule  use-stmt      { 'use' [ <version> | <module-ident> ] }
-    rule  say-stmt      { 'say' <term>? }
+    rule  say-stmt      { 'say'  <term>? }
+    rule  make-stmt     { 'make' <term>? }
+    rule  exit-stmt     { 'exit' <term>? }
     token version       { 'v' \d+ ['.' \d+]? }
     token module-ident  { <bare-symbol> ['::' <bare-symbol>]* }
     token term          { [ <symbol> | <literal> ] <indirection>? }
     token symbol        { <bare-symbol> | <sig-symbol> }
     token bare-symbol   { <ident> [ '-' <ident>? ]* }
     token sig-symbol    { <sig-twig> <bare-symbol> }
-    token sig-twig      { ['$' | '@' | '%'] ['*']? }
-    token literal       { <number> | <string> }
-    token number        { \d+ ['.' \d+]? }
-    token string        { <sq-str> }
+    token sig-twig      { ['$' | '@' | '%'] ['*' | '?']? }
+    token literal       { <number> | <q-str> }
+    token number        { '-'? \d+ ['.' \d+]? }
 
-    #----------------------------------------
-    token sq-str        { '\'' [ <-[\'\n\\]>+ | <sq-str-esc> ]* '\'' }
-    token sq-str-esc    { '\\' <[\'\\bfnrt]> }
+    #-------------------- can parse itself up to here --------------------
 
-    token indirection   { 
-        [
-        | <method-call>
-    #    | <array-index>
+    token q-str         { [ \' <s=sq-str> \' | \" <s=dq-str> \" ]
+                          { say 'q-str = ' ~ make($/{'s'}.ast).perl; }
+                        }
+
+    token sq-str {
+        [ (<-[\'\n\\]>+)
+        | \\ (<[\'\\]>)
+        | (\\ <-[\'\n\\]>)
+        ]*
+        { make $0.list.join; }
+    }
+
+    my %dq-esc = (b => "\b", r => "\r", n => "\n", f => "\f", t => "\t");
+
+    token dq-str {
+        [ (<-[\"\n\\]>+)
+        | \\ (<[\"\'\\]>)
+        | \\ (<-[\"\'\n\\]>)    { $0[*-1].make(%dq-esc{$0[*-1]} // (die 'unknow esc \\' ~ $0[*-1])) }
+        ]*
+        { make $0.map({$_.ast // $_}).list.join; }
+    }
+
+    token indirection   {
+        [ '.' <method-call>
+        | '[' <array-index> ']'
+        | '{' <hash-lookup> '}'
         ]
     }
-    token method-call   { '.' <bare-symbol> ['(' <arguments> ')' ]? }
+    token method-call   { <bare-symbol> ['(' <arguments> ')' ]? }
     rule  arguments     { [ <term> [',' <term>]* ]? }
     rule  type-decl     { <class-like> | <code-like> }
     rule  class-like    { [ 'module' | 'class' | 'grammar' ] <bare-symbol>? <class-like-body> }
@@ -45,42 +64,49 @@ grammar SelfDescribing {
     rule  rx-factor     {
         [ <rx-lit>
         | <rx-anchor>
-        | <rx-call>
+        | <rx-angle-brkt>
         | '[' <rx> ']'
         | '(' <rx> ')'
         ]
         <rx-quant>?
     }
-    token rx-call       { '<' <bare-symbol> '>' }
-    token rx-lit        { '\\' [<alpha> | '\'']  |  <string> }
+    token rx-lit        { '.'  |  '\\' [ <alpha> | <digit> | <[\'\"\\*$._]> ]  |  <q-str> }
     token rx-anchor     { '^^' | '^' | '$$' | '$' }
     token rx-quant      { '?' | '*' | '+' }
+    token rx-angle-brkt { 
+        '<'
+        [ <bare-symbol> [ '=' <bare-symbol> ]*
+        | '-'? '[' <rx-cclass> ']'
+        ]
+        '>'
+    }
+    token rx-cclass  { [ <-[\]\\]>+ | \\ . ]* }
 
 }
 
-say SelfDescribing.parse(:rule<TOP>, q:to/ENDOFTEXT/);
+say SelfDescribing.parse(:rule<TOP>, Q:to/ENDOFHEREDOC/); # uppercase Q: NO interpolation!
 use v6; # this is Perl6
-
-#use Grammar::Tracer;
-
+use Grammar::Tracer;
 
 grammar SelfDescribing {
     rule  TOP           { \s* [ <comment> | <type-decl> | <statement> ]* }
     token comment       { '#' \N* $$ }
-    rule  statement     { [ <use-stmt> | <say-stmt> | <term> ] ';' }
+    rule  statement     { [ <use-stmt> | <say-stmt> | <make-stmt>| <exit-stmt> | <term> ] ';' }
     rule  use-stmt      { 'use' [ <version> | <module-ident> ] }
-    rule  say-stmt      { 'say' <term>? }
-    token version       { 'v' <number> }
+    rule  say-stmt      { 'say'  <term>? }
+    rule  make-stmt     { 'make' <term>? }
+    rule  exit-stmt     { 'exit' <term>? }
+    token version       { 'v' \d+ ['.' \d+]? }
     token module-ident  { <bare-symbol> ['::' <bare-symbol>]* }
     token term          { [ <symbol> | <literal> ] <indirection>? }
     token symbol        { <bare-symbol> | <sig-symbol> }
     token bare-symbol   { <ident> [ '-' <ident>? ]* }
     token sig-symbol    { <sig-twig> <bare-symbol> }
-    token sig-twig      { ['$' | '@' | '%'] ['*']? }
-    token literal       { <number> | <string> }
-    token number        { \d+ ['.' \d+]? }
-    token string        { <sq-str> }
+    token sig-twig      { ['$' | '@' | '%'] ['*' | '?']? }
+    token literal       { <number> | <q-str> }
+    token number        { '-'? \d+ ['.' \d+]? }
+
+    #-------------------- can parse itself up to here --------------------
 }
-    #----------------------------------------
-ENDOFTEXT
+ENDOFHEREDOC
 
